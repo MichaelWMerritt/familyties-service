76f79fb31dfdd4c22f689cf5de2d1bb9605d136a
michaelwmerritt <michael@teuluseren.com>
2020-01-19 14:58:13 -0500

    Initial Commit

---
+++ src/main/java/com/teuluseren/services/familyties/FamilytiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teuluseren.services.familyties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilytiesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teuluseren/services/familyties/FamilytiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teuluseren.services.familyties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytiesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
